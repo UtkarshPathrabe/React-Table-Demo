fc6aa779a34ef46968b7fb6611969af10017cf25
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-02-06 12:46:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ import { PaginationTable } from './components/PaginationTable';
 import { RowSelection } from './components/RowSelection';
 import { ColumnOrder } from './components/ColumnOrder';
 import { ColumnHiding } from './components/ColumnHiding';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BasicTable />
-      <TableWithFooter />
-      <GroupedColumnsTable />
-      <SortingTable />
-      <FilteringTable />
-      <PaginationTable />
-      <RowSelection />
-      <ColumnOrder />
-      <ColumnHiding />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<BasicTable />} />
+        <Route path='/table-footer' element={<TableWithFooter />} />
+        <Route path='/grouped-columns' element={<GroupedColumnsTable />} />
+        <Route path='/sort' element={<SortingTable />} />
+        <Route path='/filter' element={<FilteringTable />} />
+        <Route path='/pagination' element={<PaginationTable />} />
+        <Route path='/row-selection' element={<RowSelection />} />
+        <Route path='/column-order' element={<ColumnOrder />} />
+        <Route path='/column-hiding' element={<ColumnHiding />} />
+      </Routes>
     </div>
   );
 }
